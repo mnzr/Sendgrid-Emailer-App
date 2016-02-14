00d170e8b26512a689d8c4739f219067acc434de
Ratul Minhaz <minhaz.ratul@gmail.com>
2016-02-14 17:47:19 +0600

    Added WYSIWYG editor

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,12 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField
 from flask_wysiwyg.wysiwyg import WysiwygField
-
+from flask_wtf.file import FileField
 
 class EmailForm(Form):
-    email_addresses = StringField('Email Addresses')
+    email_addresses = StringField('Email addresses')
+    email_upload = FileField('Email address list upload')
     subject = StringField('Subject')
-    body = WysiwygField('body')
+    body = WysiwygField('Body')
 
     #TextAreaField('Message')
