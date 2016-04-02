dad0fa75883674ceb0310b060c0b291491662319
Ratul Minhaz <minhaz.ratul@gmail.com>
2016-04-02 16:35:57 +0600

    Added templates folder to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask
+
+project_root = os.path.dirname(__file__)
+template_path = os.path.join(project_root, 'app/templates')
 # Creates an object called app of Flask class
-app = Flask(__name__)
+app = Flask(__name__, template_folder=template_path)
 app.config.from_object('config')
 
 # Imports app package from views, not the same as object 'app'
