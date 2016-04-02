4259fea7f75e4c7c08eb2ede690c240d86100f8b
Ratul Minhaz <minhaz.ratul@gmail.com>
2016-04-02 16:46:08 +0600

    Fixed template relative path

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 project_root = os.path.dirname(__file__)
-template_path = os.path.join(project_root, 'app/templates')
+template_path = os.path.join(project_root, 'templates')
 # Creates an object called app of Flask class
 app = Flask(__name__, template_folder=template_path)
 app.config.from_object('config')
